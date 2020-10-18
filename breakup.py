435304d20cbe4105bf5398210a6fe76bc494aa27
sampathpavan <73058239+sampathpavan@users.noreply.github.com>
2020-10-18 03:12:12 -0700

    Add files via upload

---
+++ breakup.py
@@ -0,0 +1,14 @@
+def breakup_motivation():
+	m=["1.) Fistly you have to realize that he/she will be not be there with you",
+	"2.) Set your environment peace and healthy",
+	"3.) Watch some comical videos to make yourself laugh",
+	"4.) At the end of the day you must realize that the life is yours not your loved ones",
+	"5.) Make him/her a beautiful memory in your life and move ahead",
+	"6.) You must set new goals to come out of that sad thing and get focussed on it",
+	"7.) You must just move on thinking that life will give you a second option",
+	"8.) Hope good and continue your life and search for the best",
+	"9.) Finally remember that life is filled with both ups and downs"]
+	for i in m:
+		print(i)
+
+
---
+++ workfailure.py
@@ -0,0 +1,10 @@
+def workfailure_motivation():
+    l=["1.) You must have self confidence,faith on yourselves",
+    "2.) Divide your goals into small small goals then it will be easier to go further",
+    "3.) God treats all the people equally and gives each people 50%percent success and 50% failures",
+    "4.) It is a bit surprising to hear but the fact is whenever u got failure u shouls be remained happy because as we have 50% failures the percentage of failure decreases",
+    "5.) Prepare yourself a neat and friendly environment",
+    "6.) Working will be in our hands not the result.so enjoy the work you are doing and one very fine day you will automatically get the success",
+    "7.) Remember success will get from failure"]
+    for i in l:
+    	print(i)
