08126d13983eb24509e4f4814903b679bb792485
srivatsa2 <72606165+srivatsa2@users.noreply.github.com>
2020-10-18 15:42:24 +0530

    Add files via upload

---
+++ friend loss_motivation.py
@@ -0,0 +1,9 @@
+def friendlost_grief():
+    print("1.i am sorry for your lose,i wish you i have a great words i will care about you")
+    print("2.i dont know how you feel but i am hear to you be as a friend")
+    print("_DONT FEEL BAD_")
+    print("3.give a hug to feel better dude")
+    print("4.dont feel  iam single i am hear to do any help for u")
+    print("__FORGET PAST AND ENJOY THE PRESENT SITUATIONS_")
+    print()
+    return 0
---
+++ parent loss_motivation.py
@@ -0,0 +1,9 @@
+def parentlost_grief():
+    print("1.i am so sorry to hear about your parents, may your parents be REST IN PEACE")
+    print("2.thinking of u in these difficult times")
+    print("3.he/she may be in better place,losing such a strong person in life is one of the hardest thing ever")
+    print("4.the passing of one who introduce us to the planet and who loved us along path is never easy,but we have to accept all consequences")
+    print("5.bond between you and your parents was strong.even death cannot do them in differnt parts")
+    print("_LETS BE STRONG__")
+    print()
+    return 0
